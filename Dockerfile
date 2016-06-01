6a5da2d05e756cbd855e7d2bc0e550e007cafa35
Stu Schwartz <stupschwartz@gmail.com>
2016-05-31 21:40:54 -0400

    docker compose setup

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:2.7
+
+RUN mkdir /opt/branching
+
+WORKDIR /opt/branching
--- src/branching.py
+++ src/branching.py
@@ -1,4 +1,11 @@
+import os
 
+class Branch(object):
+    def __init__(self, name):
+        self.name = name
 
+    def push(self):
+        os.system('git push origin %s' % self.name)
 
+print 'branching'
 
